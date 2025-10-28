5459c3a617c99197bbf925bbb6d57289b1aa6e04
alexcalder93-web <alex.calder93@gmail.com>
2025-10-28 16:41:59 +0000

    Update redm_med_bot.py

--- redm_med_bot.py
+++ redm_med_bot.py
@@ -9,19 +9,6 @@ import discord
 from discord import app_commands
 from google.oauth2.service_account import Credentials
 import gspread
-from flask import Flask
-from threading import Thread
-
-app = Flask("")
-
-@app.route("/")
-def home():
-    return "Bot running"
-
-def run():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-Thread(target=run).start()
 
 
 # ---------- Config ----------
